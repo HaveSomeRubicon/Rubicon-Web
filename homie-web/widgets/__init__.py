ba23ba2f8e9e14df7b922c0de5a6b6cb4b42b792
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-09-25 22:45:24 -0400

    Move MainWindow class to widgets/main_window.py

--- homie-web/widgets/__init__.py
+++ homie-web/widgets/__init__.py
@@ -2,3 +2,4 @@ from .load_progress_bar import LoadProgressBar
 from .navbar import NavBar
 from .tabbed_browser import TabbedBrowser
 from .web_engine import WebEngineView, WebEnginePage
+from .main_window import MainWindow
