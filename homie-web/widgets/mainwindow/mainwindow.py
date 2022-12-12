549006c1529e734f22bc372a3a60453206f39378
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-12-11 20:52:18 -0500

    Get rid of unecessary imports

--- homie-web/widgets/mainwindow/mainwindow.py
+++ homie-web/widgets/mainwindow/mainwindow.py
@@ -2,7 +2,7 @@ import os
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QLabel, QTabBar
+from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUi
 
 
