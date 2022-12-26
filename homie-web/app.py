2369494490d5afde39f4febac63453c88f9367c6
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-12-26 10:38:01 -0500

    Give config as argument to MainWindow

--- homie-web/app.py
+++ homie-web/app.py
@@ -3,12 +3,13 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.uic import loadUi
 
+from config import config
 from widgets.mainwindow.mainwindow import MainWindow
 
 
 app = QApplication(sys.argv)
 
-window = MainWindow()
+window = MainWindow(config)
 window.show()
 
 app.exec()
