af36c5a251708a86d28bce97c8650a66ea7a5690
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-12-26 21:57:48 -0500

    Rename web_views to tab_widgets

--- homie-web/widgets/web_engine/web_engine.py
+++ homie-web/widgets/web_engine/web_engine.py
@@ -23,8 +23,8 @@ class WebEngineView(QWebEngineView):
     
     def createWindow(self, window_type: QWebEnginePage.WebWindowType) -> 'QWebEngineView':
         if window_type == QWebEnginePage.WebWindowType.WebBrowserTab:
-            return self.main_window.web_views.widget(self.main_window.new_web_view_tab())
+            return self.main_window.tab_widgets.widget(self.main_window.new_web_view_tab())
         elif window_type == QWebEnginePage.WebWindowType.WebBrowserBackgroundTab:
-            return self.main_window.web_views.widget(self.main_window.new_web_view_tab(background = True))
+            return self.main_window.tab_widgets.widget(self.main_window.new_web_view_tab(background = True))
         
         return super().createWindow(window_type)
