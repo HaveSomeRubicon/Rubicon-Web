9672fe888bae62c723ca789f470c43a772d11ecb
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2023-01-07 13:48:41 -0500

    Focus text on Urlbar focus

--- homie-web/widgets/top_bar/nav_bar/url_bar/url_bar.py
+++ homie-web/widgets/top_bar/nav_bar/url_bar/url_bar.py
@@ -11,6 +11,8 @@ class UrlBar(QLineEdit):
         
         self.main_window = main_window
         
+        self.focused = False
+        
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
         self.setPlaceholderText("Search or type a URL")
     
@@ -20,3 +22,15 @@ class UrlBar(QLineEdit):
     def update_url(self, qurl: QUrl, browser: WebEngineView or QWebEngineView):
         if browser == self.main_window.tab_widgets.widget(self.main_window.top_bar.tab_bar.tabs.currentIndex()):
             self.update_text(qurl.toString())
+    
+    def mousePressEvent(self, event) -> None:
+        super(UrlBar, self).mousePressEvent(event)
+        print("MousePressEvent" + str(self.focused))
+        if self.focused == False:
+            self.selectAll()
+            self.focused = True
+    
+    def focusOutEvent(self, event) -> None:
+        super(UrlBar, self).focusOutEvent(event)
+        print("FocusOutEvent" + str(self.focused))
+        self.focused = False
