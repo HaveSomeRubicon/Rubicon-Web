b6fa566f5d0f2a947a7fef5c591678c4fe789858
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2023-01-09 19:22:35 -0500

    Implement if __name__ = "__main__"

--- rubicon_web/rubicon-web.py
+++ rubicon_web/rubicon-web.py
@@ -7,9 +7,14 @@ from utils.configutils import get_config
 from utils.themeutils import get_theme
 
 
-app = QApplication(sys.argv)
+def main():
+    app = QApplication(sys.argv)
+    
+    window = MainWindow(get_config(), get_theme())
+    window.show()
+    
+    app.exec()
 
-window = MainWindow(get_config(), get_theme())
-window.show()
 
-app.exec()
+if __name__ == "__main__":
+    main()
