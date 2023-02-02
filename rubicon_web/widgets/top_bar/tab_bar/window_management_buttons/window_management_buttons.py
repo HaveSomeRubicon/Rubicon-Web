5e93a1c1aa32f0cd18c4ff53caf402dfcd1f5a6d
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2023-02-01 19:23:03 -0500

    Use Ui_mainwindow.py instead of Ui_mainwindow.ui

--- rubicon_web/widgets/top_bar/tab_bar/window_management_buttons/window_management_buttons.py
+++ rubicon_web/widgets/top_bar/tab_bar/window_management_buttons/window_management_buttons.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QWidget
 from widgets.top_bar.tab_bar.window_management_buttons.Ui_window_management_buttons import Ui_window_management_buttons
 
 
-class WindowManagementButtons(QWidget):
+class WindowManagementButtons(QWidget, Ui_window_management_buttons):
     def __init__(self, *args, **kwargs):
         super(QWidget, self).__init__(*args, **kwargs)
-        loadUi("rubicon_web/widgets/top_bar/tab_bar/window_management_buttons/window_management_buttons.ui", self)
+        self.setupUi(self)
