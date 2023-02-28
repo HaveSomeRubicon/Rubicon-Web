7b338bd5cd2b69ef1a86dbaa086fa026a1adcc9f
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2023-02-28 15:01:12 -0500

    Combine Ui_tab_bar.ui into tab_bar.py

--- rubicon_web/widgets/top_bar/tab_bar/tab_bar.py
+++ rubicon_web/widgets/top_bar/tab_bar/tab_bar.py
@@ -1,19 +1,50 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QSizePolicy, QHBoxLayout
 from PyQt5.QtCore import Qt
 
-from widgets.top_bar.tab_bar.Ui_tab_bar import Ui_tab_bar
 from widgets.top_bar.tab_bar.tabs.tabs import Tabs
 from widgets.top_bar.tab_bar.window_management_buttons.window_management_buttons import WindowManagementButtons
 
 
-class TabBar(QWidget, Ui_tab_bar):
+class TabBar(QWidget):
     def __init__(self, parent, *args, **kwargs):
         super(TabBar, self).__init__(parent=parent, *args, **kwargs)
         self.main_window = self.parent().parent()
         self.main_window.log("TabBar is being initialized", "OKAY", "tab_bar.py")
-        self.setupUi(self)
 
+        self.setObjectName("tab_bar")
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
         self.setAttribute(Qt.WA_StyledBackground, True)
+        
+        self.setStyleSheet("""QWidget {
+    background-color: /tab_bar_bg_color/;
+}
+QTabBar {
+    background: /tab_bar_bg_color/;
+    border: 0px;	
+    color: white;
+}
+QTabBar::tab {
+    background: /tab_bg_color/;
+    border: 2px solid /tab_bar_accent_color/;
+    border-radius: 6px;
+    padding: 4px;
+    margin: 2px;
+    color: /tab_font_color/;
+}
+QTabBar::tab:hover {
+    background-color: /tab_hover_color/;
+}
+QTabBar::tab:selected {
+    background: /tab_focus_color/;
+}
+QTabBar::close-button {
+    image: url(*rubicon_web/widgets/top_bar/tab_bar/tabs/svg/close tab button icon.svg*);
+}""")
+
+        self.tab_bar_layout = QHBoxLayout(self)
+        self.tab_bar_layout.setObjectName("tab_bar_layout")
+        self.tab_bar_layout.setContentsMargins(2, 4, 2, 2)
+        self.tab_bar_layout.setSpacing(0)
 
         self.tabs = Tabs(self)
         self.tab_bar_layout.addWidget(self.tabs)
@@ -21,4 +52,5 @@ class TabBar(QWidget, Ui_tab_bar):
         if self.main_window.get_configuration()["show_window_management_buttons"]:
             self.window_management_buttons = WindowManagementButtons(self)
             self.tab_bar_layout.addWidget(self.window_management_buttons)
+        
         self.main_window.log("TabBar has been initialized", "SUCCESS", "tab_bar.py")
